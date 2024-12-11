a9fa8c261bb6d0e1b5528605fd30431e668a812f
Maxim Danilov <abl1v1on@yandex.ru>
2024-12-12 00:17:44 +0500

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,54 @@
+import random
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def get_fastapi_app() -> FastAPI:
+    app = FastAPI(
+        docs_url=None,
+        redoc_url=None,
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            'http://localhost:5173', 
+            'http://127.0.0.1:5173'
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    return app
+
+
+app = get_fastapi_app()
+
+items = [
+    {
+        'id': 1,
+        'name': 'Docker',
+        'url': 'https://static-00.iconduck.com/assets.00/docker-icon-2048x2048-5mc7mvtn.png'
+    },
+    {
+        'id': 2,
+        'name': 'GitHub',
+        'url': 'https://cdn-icons-png.flaticon.com/512/25/25231.png'
+    },
+    {
+        'id': 3,
+        'name': 'Nginx',
+        'url': 'https://www.svgrepo.com/show/373924/nginx.svg'
+    }
+]
+
+
+@app.get('/items')
+async def get_items():
+    shuffled_items = items.copy()
+    random.shuffle(shuffled_items)
+    return shuffled_items
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
---
+++ deploy-frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios";
+import './App.css'
+
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  const baseUrl = 'http://localhost:8000';
+
+  const fetchItems = () => {
+    axios.get(`${baseUrl}/items`)
+    .then(response => {
+      const responseItems = response.data;
+      setItems(responseItems);
+    })
+    .catch(errors => {
+      console.log(errors);
+    });
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <div className="items">
+      {items.map(item => (
+        <div key={item.id} className="item">
+          <img width={150} src={item.url} alt="" />
+          <p>{item.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
